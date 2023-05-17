6d21cf5ff1bc868b9fda5b1b8b6197be78234e05
AnaKostova <anakostova13@gmail.com>
2023-05-17 23:31:10 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {System.out.println("Hello World");
+	// write your code here
+
+
+
+
+        sayHello("Mimi");
+    }
+
+    public static void sayHello(String name) {System.out.println("Hello" +name);}
+
+
+
+
+
+
+    }
+
+
